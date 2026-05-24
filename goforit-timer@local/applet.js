0e60edc4c14fba2f4e22b3fa70e996ff0b64103e
xyz <1216407-ntop@users.noreply.gitlab.com>
2026-05-23 21:10:34 -0400

    Migrate to ES6 class syntax and support Cinnamon 6.6+
    Replace deprecated prototype-based pattern and Lang.bind with ES6 class
    and arrow functions for compatibility with Cinnamon 6.6+ (Mint 22.3).
    Update cinnamon-version metadata to include 6.6 and 6.8.

--- goforit-timer@local/applet.js
+++ goforit-timer@local/applet.js
@@ -1,46 +1,40 @@
 const Applet = imports.ui.applet;
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
-const Lang = imports.lang;
 
 const DBUS_NAME = 'com.github.jmoerman.goforit.Timer';
 const DBUS_PATH = '/com/github/jmoerman/goforit/Timer';
 const DBUS_IFACE = 'com.github.jmoerman.goforit.Timer';
 
-function MyApplet(orientation, panel_height, instance_id) {
-    this._init(orientation, panel_height, instance_id);
-}
+class MyApplet extends Applet.TextApplet {
+    constructor(metadata, orientation, panelHeight, instanceId) {
+        super(orientation, panelHeight, instanceId);
 
-MyApplet.prototype = {
-    __proto__: Applet.TextApplet.prototype,
+        this.set_applet_label('--:--');
+        this.set_applet_tooltip('Go-For-It! Timer');
 
-    _init: function(orientation, panel_height, instance_id) {
-        Applet.TextApplet.prototype._init.call(this, orientation, panel_height, instance_id);
-        
-        this.set_applet_label("--:--");
-        this.set_applet_tooltip("Go-For-It! Timer");
-        
         this._proxy = null;
         this._remainingSeconds = 0;
         this._timerState = 'stopped';
-        
-        this._setupDBus();
-        
-        this._refreshInterval = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1, 
-            Lang.bind(this, this._refreshState));
-    },
 
-    _setupDBus: function() {
+        this._setupDBus();
+
+        this._refreshInterval = GLib.timeout_add_seconds(
+            GLib.PRIORITY_DEFAULT, 1, () => this._refreshState()
+        );
+    }
+
+    _setupDBus() {
         Gio.bus_watch_name(
             Gio.BusType.SESSION,
             DBUS_NAME,
             Gio.BusNameWatcherFlags.NONE,
-            Lang.bind(this, this._onBusAcquired),
-            Lang.bind(this, this._onBusVanished)
+            (connection, name) => this._onBusAcquired(connection, name),
+            (connection, name) => this._onBusVanished(connection, name)
         );
-    },
+    }
 
-    _onBusAcquired: function(connection, name) {
+    _onBusAcquired(connection, name) {
         try {
             this._proxy = Gio.DBusProxy.new_for_bus_sync(
                 Gio.BusType.SESSION,
@@ -51,20 +45,22 @@ MyApplet.prototype = {
                 DBUS_IFACE,
                 null
             );
-            
-            this._proxy.connect('g-signal', Lang.bind(this, this._onDBusSignal));
+
+            this._proxy.connect('g-signal', (proxy, senderName, signalName, params) => {
+                this._onDBusSignal(proxy, senderName, signalName, params);
+            });
             this._refreshState();
         } catch (e) {
             this._proxy = null;
         }
-    },
+    }
 
-    _onBusVanished: function(connection, name) {
+    _onBusVanished(connection, name) {
         this._proxy = null;
         this.set_applet_label('--:--');
-    },
+    }
 
-    _onDBusSignal: function(proxy, senderName, signalName, params) {
+    _onDBusSignal(proxy, senderName, signalName, params) {
         if (signalName === 'TimeChanged') {
             this._remainingSeconds = params.get_child_value(0).get_uint32();
             this._updateDisplay();
@@ -72,11 +68,11 @@ MyApplet.prototype = {
             this._timerState = params.get_child_value(0).get_string()[0];
             this._updateDisplay();
         }
-    },
+    }
 
-    _refreshState: function() {
+    _refreshState() {
         if (!this._proxy) return GLib.SOURCE_CONTINUE;
-        
+
         try {
             let variant = this._proxy.call_sync(
                 'org.freedesktop.DBus.Properties.Get',
@@ -84,31 +80,31 @@ MyApplet.prototype = {
                 Gio.DBusCallFlags.NONE, -1, null
             );
             this._remainingSeconds = variant.get_child_value(0).get_variant().get_uint32();
-            
+
             variant = this._proxy.call_sync(
                 'org.freedesktop.DBus.Properties.Get',
                 GLib.Variant.new('(ss)', [DBUS_IFACE, 'State']),
                 Gio.DBusCallFlags.NONE, -1, null
             );
             this._timerState = variant.get_child_value(0).get_variant().get_string()[0];
-            
+
             this._updateDisplay();
         } catch (e) {}
-        
-        return GLib.SOURCE_CONTINUE;
-    },
 
-    _updateDisplay: function() {
+        return GLib.SOURCE_CONTINUE;
+    }
+
+    _updateDisplay() {
         let mins = Math.floor(this._remainingSeconds / 60);
         let secs = this._remainingSeconds % 60;
         let formattedTime = mins.toString().padStart(2, '0') + ':' + secs.toString().padStart(2, '0');
         this.set_applet_label(formattedTime);
         this.set_applet_tooltip('Go-For-It! Timer - ' + this._timerState);
-    },
+    }
 
-    on_applet_clicked: function(event) {
+    on_applet_clicked(event) {
         if (!this._proxy) return true;
-        
+
         try {
             if (this._timerState === 'running') {
                 this._proxy.call_sync('Stop', null, Gio.DBusCallFlags.NONE, -1, null);
@@ -116,17 +112,17 @@ MyApplet.prototype = {
                 this._proxy.call_sync('Start', null, Gio.DBusCallFlags.NONE, -1, null);
             }
         } catch (e) {}
-        
-        return true;
-    },
 
-    on_applet_removed_from_panel: function() {
+        return true;
+    }
+
+    on_applet_removed_from_panel() {
         if (this._refreshInterval) {
             GLib.source_remove(this._refreshInterval);
         }
     }
-};
+}
 
-function main(metadata, orientation, panel_height, instance_id) {
-    return new MyApplet(orientation, panel_height, instance_id);
+function main(metadata, orientation, panelHeight, instanceId) {
+    return new MyApplet(metadata, orientation, panelHeight, instanceId);
 }
